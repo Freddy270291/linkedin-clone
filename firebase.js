f7ce1f729ada75f2f3c3a33f0f01715084ab05b5
Federico Giovannini <giovannini.federico@hotmail.it>
2021-06-10 20:11:04 +0200

    first commit

---
+++ firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAA33QRvgMb2blkI8NgjDYvXa9GbIRcQJk",
+  authDomain: "linkedin-clone-62e5f.firebaseapp.com",
+  projectId: "linkedin-clone-62e5f",
+  storageBucket: "linkedin-clone-62e5f.appspot.com",
+  messagingSenderId: "784065846542",
+  appId: "1:784065846542:web:a3843a003a78bbe1f64b5f",
+};
