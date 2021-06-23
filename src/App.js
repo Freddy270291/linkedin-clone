2f82282b176267d7af364d2c8ae8a2989d85016d
Federico Giovannini <giovannini.federico@hotmail.it>
2021-06-23 20:52:30 +0200

    Authentication

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react";
+import { useSelector } from "react-redux";
 import "./App.css";
 import Feed from "./Components/Feed";
 import Header from "./Components/Header";
 import Sidebar from "./Components/Sidebar";
+import { selectUser } from "./features/userSlice";
+import Login from "./Login";
 
 function App() {
+  const user = useSelector(selectUser);
   return (
     <div className='app'>
       <Header />
-      {/* App Body */}
-      <div className='app__body'>
-        <Sidebar />
-        <Feed />
-        {/* Right Side */}
-      </div>
+
+      {!user ? (
+        <Login />
+      ) : (
+        <div className='app__body'>
+          <Sidebar />
+          <Feed />
+          {/* Right Side */}
+        </div>
+      )}
     </div>
   );
 }
