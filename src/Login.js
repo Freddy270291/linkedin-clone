f301a2522776716179abc8a791054609ac4292a8
Federico Giovannini <giovannini.federico@hotmail.it>
2021-06-24 20:38:02 +0200

    Login page

--- src/Login.js
+++ src/Login.js
@@ -1,9 +1,67 @@
-import React from "react";
+import React, { useState } from "react";
+import { auth } from "./Components/firebase";
 import "./Login.css";
 
 function Login() {
-  return <div className='login'></div>;
+  const [email, SetEmail] = useState("");
+  const [name, SetName] = useState("");
+  const [password, SetPassword] = useState("");
+  const [profilePic, SetProfilePic] = useState("");
+
+  const loginToApp = (e) => {
+    e.preventDefault();
+  };
+
+  const register = () => {};
+
+  return (
+    <div className='login'>
+      <img
+        src='https://news.hitb.org/sites/default/files/styles/large/public/field/image/500px-LinkedIn_Logo.svg__1.png?itok=q_lR0Vks'
+        alt=''
+      />
+
+      <form>
+        <input
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          type='text'
+          placeholder='Full Name (Required if registering)'
+        />
+        <input
+          value={profilePic}
+          onChange={(e) => setProfilePic(e.target.value)}
+          type='text'
+          placeholder='Profile pic URL (optional)'
+        />
+        <input
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+          type='email'
+          placeholder='Email'
+        />
+        <input
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          type='password'
+          placeholder='Password'
+        />
+
+        <button type='submit' onClick={loginToApp} className='login__button'>
+          Sign In
+        </button>
+      </form>
+
+      <p>
+        Not a member?{" "}
+        <span className='login__register' onClick={register}>
+          Register Now
+        </span>
+      </p>
+    </div>
+  );
 }
 
 export default Login;
 
+// 3:05:30
